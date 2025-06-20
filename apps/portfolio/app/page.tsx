1d9a1de72a512fe2ecf417d98c3300883061bd34
yunahya <yunah.kimya@i-esg.io>
2025-06-20 17:01:04 +0900

    Test production branch

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -8,6 +8,7 @@ export default function Page() {
   return (
     <PortfolioLayout>
       <>
+        {/*  */}
         <AboutSection id="about" />
         <SkillsSection id="skills" />
         <ExperienceSection id="experience" />
